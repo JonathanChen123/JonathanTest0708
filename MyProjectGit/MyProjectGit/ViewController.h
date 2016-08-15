861ffc43e2a55b2bfbf09ae0becff23edf277316
JonathanChen123 <chenjiaqi@chenjiaqideMacBook-Pro.local>
2016-08-16 06:45:55 +0800

    new

---
+++ MyProjectGit/MyProjectGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyProjectGit
+//
+//  Created by 陈佳期 on 16/8/16.
+//  Copyright © 2016年 JC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyProjectGit/MyProjectGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyProjectGit
+//
+//  Created by 陈佳期 on 16/8/16.
+//  Copyright © 2016年 JC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
